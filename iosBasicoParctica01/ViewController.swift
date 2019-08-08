3dc05c7cf99c837799af7d38db8289c1c6ae13f0
JorgeVC87 <lacunacoil18@hotmail.com>
2019-08-07 22:55:42 -0500

    impl

---
+++ iosBasicoParctica01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosBasicoParctica01
+//
+//  Created by MacBooK Pro on 8/7/19.
+//  Copyright © 2019 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
